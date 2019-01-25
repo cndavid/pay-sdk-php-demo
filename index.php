eaaaa09feac92d09146ed803bf6348cbc1db7998
David <31339991@qq.com>
2019-01-25 16:17:41 +0800

    test

--- index.php
+++ index.php
@@ -1,22 +1,33 @@
+
+
+
 <?php
+if($_REQUEST){
+    require_once "pay.php";
 
-require_once "pay.php";
+    $data = [
+        "order_no" => date('Ydmhis') . time(),
+        "body" => '商品',
+        "sum" => $_REQUEST['sum'],
+        "channel" => strtolower('alipay2alipay'), //渠道  官方的official
+        "extra" => []
+    ];
+
+    $pay = new Payment($config);
+    $result = $pay->pay($data);  // ->pay(); ->callback();
+
+    exit(json_encode($result));
+}else{
+ ?>
+    <form method="post">
+        充值金额：<input type="text" name="sum"></br>
+        充值渠道:<select name="channel"><option value="alipay2alipay">支付宝转支付宝</option></select></br>
+        <input type="submit">
+    </form>
 
 
-$data = [
-    "order_no" => date('Ydmhis') . time(),
-    "body" => '商品Ａ',
-    "sum" => '1.00',
-    "channel" => strtolower('alipay2alipay'), //渠道  官方的official
-    "extra" => [
-        "openid"  => 'oWbNY5LQY2E_U0BH06v_4XGWy_I0'
-    ]
-];
-
-$pay = new Payment($config);
-$result = $pay->pay($data);  // ->pay(); ->callback();
-
-exit(json_encode($result));
-
+<?php
+}
+?>
 
 
